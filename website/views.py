de64073984aa6c7f695fdedb628ce7ca606f4ac2
Yardy Nguyen <tintokaka@gmail.com>
2023-05-18 01:07:10 -0700

    .

---
+++ website/views.py
@@ -0,0 +1,11 @@
+# front end
+# log-in, home, and log=out pages
+
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+
+def home():
+    return render_template("home.html")
